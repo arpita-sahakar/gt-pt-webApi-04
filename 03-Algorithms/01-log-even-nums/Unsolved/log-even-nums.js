32f883e706c93dae375bfcc0e7d30da3cefe2642
Arpita Saha <arpita.sahaa@gmail.com>
2021-03-30 10:54:51 -0400

    completed algo of unsolved

--- 03-Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
+++ 03-Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,15 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+// var logEvenNums = function(num) {};
+
+const logEvenNums = (num) => {
+  for (i = 0; i <= num; i++) {
+      if(i%2===0){
+        console.log(i);
+      }
+    
+  }
+};
+
+logEvenNums(20);
--- 03-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ 03-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,17 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+// var sumArray = function(arr) {};
+
+
+const sumArray = (arrOfNum)=>{
+    let sum = 0;
+    for (i=0; i<arrOfNum.length;i++){
+        sum = sum + arrOfNum[i];
+        
+    }
+    console.log(sum);
+}
+
+ 
+
+sumArray([10,5,4,3]);  
